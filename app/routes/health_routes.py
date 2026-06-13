8862430e40843ff1cd932bbb39548b9dc7f5d594
alvarolopezmoya <72213966+alvarolopezmoya@users.noreply.github.com>
2026-06-13 14:32:57 +0000

    :comit "todo lo todeable"

---
+++ app/routes/health_routes.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+
+from app.schemas.health_check_models import HealthCheck
+from app.services.health_services import HealthServices
+
+health_router = APIRouter(prefix="/health", tags=["Health"])
+health = HealthServices()
+
+
+
+@health_router.get("/")
+def get_health_api () -> HealthCheck:
+    
+    return health.get_health()
